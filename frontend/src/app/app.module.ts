73c34e08a094ef77764479e983cf18ae14206de5
SahnounYusuf <sahnounyusuf@outlook.fr>
2022-07-18 01:29:10 +0100

    template added

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,12 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './components/home/footer/footer.component';
+import { NewsComponent } from './components/home/news/news.component';
+import { FaqComponent } from './components/home/faq/faq.component';
+import { PricingComponent } from './components/home/pricing/pricing.component';
+import { FeaturesComponent } from './components/home/features/features.component';
+import { HeaderComponent } from './components/home/header/header.component';
+import { NavbarComponent } from './components/home/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    FooterComponent,
+    NewsComponent,
+    FaqComponent,
+    PricingComponent,
+    FeaturesComponent,
+    HeaderComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
