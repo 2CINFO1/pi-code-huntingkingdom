a2b2e16e63929ba856471abd56892fedaa9f420a
lkhlEya <86523827+lkhlEya@users.noreply.github.com>
2022-07-19 00:51:14 +0100

    forest home page ready to be used :)

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,24 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FooterComponent } from './components/home/footer/footer.component';
-import { NewsComponent } from './components/home/news/news.component';
-import { FaqComponent } from './components/home/faq/faq.component';
-import { PricingComponent } from './components/home/pricing/pricing.component';
-import { FeaturesComponent } from './components/home/features/features.component';
-import { HeaderComponent } from './components/home/header/header.component';
-import { NavbarComponent } from './components/home/navbar/navbar.component';
+import { NavbarComponent } from './components/common/navbar/navbar.component';
+import { CategoryBarComponent } from './components/common/category-bar/category-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FooterComponent,
-    NewsComponent,
-    FaqComponent,
-    PricingComponent,
-    FeaturesComponent,
-    HeaderComponent,
-    NavbarComponent
+    NavbarComponent,
+    CategoryBarComponent
   ],
   imports: [
     BrowserModule,
