ca2d59f9e4065403660a5480c919ce307b45f121
wissal <wissal.yermani1@esprit.tn>
2022-07-19 23:29:08 +0200

    blog changes

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,20 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {HttpClientModule} from "@angular/common/http";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/common/navbar/navbar.component';
 import { CategoryBarComponent } from './components/common/category-bar/category-bar.component';
+import { BlogListComponent } from './components/blog/blog-list/blog-list.component';
+import { BlogAddComponent } from './components/blog/blog-add/blog-add.component';
+import { FormsModule } from '@angular/forms';
+import { BlogDashboardComponent } from './components/blog/blog-dashboard/blog-dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    CategoryBarComponent
+    CategoryBarComponent,
+    BlogListComponent,
+    BlogAddComponent,
+    BlogDashboardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
