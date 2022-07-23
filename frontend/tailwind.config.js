a3d88aaee7155aacc33a809c65afbae8effe6dc6
Wild4Hell <79971547+Wild4Hell@users.noreply.github.com>
2022-07-23 20:55:26 +0100

    Dashboard links

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,6 +3,7 @@ module.exports = {
   content: [
     "src/app/views/user/user-products/**/*.{html,ts}",
     "src/app/views/**/**/**/**/*.{html,ts}",
+    "src/app/layouts/**/*.{html,ts}",
   ],
   theme: {
     extend: {},
